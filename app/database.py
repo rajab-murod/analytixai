5dcb318d9ab79cc56370e1f65280ca379977cab9
rajab-murod <mr.header1@gmail.com>
2026-08-16 10:15:51 +0500

    changed config turso

--- app/database.py
+++ app/database.py
@@ -5,20 +5,19 @@ from sqlalchemy.orm import sessionmaker
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DB_PATH = os.path.join(BASE_DIR, "platform.db")
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 TURSO_URL = os.environ.get("TURSO_DATABASE_URL")
 TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
 
-if TURSO_URL:
-    SQLALCHEMY_DATABASE_URL = f"sqlite+{TURSO_URL.replace('libsql://', 'libsql://')}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
+if TURSO_URL and TURSO_AUTH_TOKEN:
+    # Token URL ichida emas, connect_args orqali beriladi
+    SQLALCHEMY_DATABASE_URL = f"sqlite+{TURSO_URL}?secure=true"
+    connect_args = {"auth_token": TURSO_AUTH_TOKEN}
 else:
     SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"  # lokal development
+    connect_args = {"check_same_thread": False}
 
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
