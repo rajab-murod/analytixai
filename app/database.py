73a2b8048b97472c7e994e6f8eb33dce916a9211
rajab-murod <mr.header1@gmail.com>
2026-08-16 09:43:19 +0500

    config turso

--- app/database.py
+++ app/database.py
@@ -7,6 +7,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DB_PATH = os.path.join(BASE_DIR, "platform.db")
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
+TURSO_URL = os.environ.get("TURSO_DATABASE_URL")
+TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
+
+if TURSO_URL:
+    SQLALCHEMY_DATABASE_URL = f"sqlite+{TURSO_URL.replace('libsql://', 'libsql://')}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
+else:
+    SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"  # lokal development
+
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
